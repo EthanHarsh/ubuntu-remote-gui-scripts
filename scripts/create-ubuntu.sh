5752114643d900fb04493031ba4041d6bcabd36b
Ethan Harsh <ethan@ethanharsh.com>
2022-10-03 20:42:33 +0000

    init

---
+++ scripts/create-ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get --with-new-pkgs upgrade -y
+sudo apt autoremove -y
+sudo apt install --assume-yes wget tasksel
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
+sudo tasksel install ubuntu-desktop
+sudo bash -c ‘echo “exec /etc/X11/Xsession /usr/bin/gnome-session” > /etc/chrome-remote-desktop-session’
+sudo reboot
